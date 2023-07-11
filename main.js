8dc7ce2cd25b046ed5154ed680c7f8863145924f
Erol Akyildiz <erol.akyildiz@sebit.com.tr>
2023-07-11 14:46:37 +0300

    Displayin Books + Toast

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 import "./style.css";
 
 const formSelector = document.querySelector("form");
-const bookListSelector = document.querySelector(".book-list");
+const bookListRef = document.getElementById("book-list");
 
 let bookList = [];
 
 const addBook = (book) => {
   bookList = [...bookList, book];
+};
+
+const deleteBook = (isbn) => {
+  bookList = bookList.filter((book) => book.isbn !== isbn);
+};
+
+const toast = (message) => {
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+
+  const className =
+    message === "Book added successfully" ? "success" : "delete";
+  toast.classList.add(`toast-${className}`);
+
+  toast.innerText = message;
+
+  document.body.appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 3000);
 };
 
 formSelector.addEventListener("submit", (e) => {
@@ -25,4 +46,42 @@ formSelector.addEventListener("submit", (e) => {
   };
 
   addBook(book);
+  displayBooks();
+  toast("Book added successfully");
+
+  e.target.reset();
 });
+
+const displayBooks = () => {
+  bookListRef.innerHTML = "";
+
+  bookList.forEach((book) => {
+    const bookItem = document.createElement("tr");
+
+    bookItem.classList.add("book-item");
+
+    bookItem.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td class="action">
+            ${book.isbn}
+            <button class="btn btn-delete" data-isbn="${book.isbn}">X</button>
+        </td>
+        `;
+
+    bookListRef.appendChild(bookItem);
+  });
+
+  const deleteBtns = document.querySelectorAll(".btn-delete");
+
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const isbn = e.target.dataset.isbn;
+      deleteBook(isbn);
+      displayBooks();
+      toast("Book deleted successfully");
+    });
+  });
+};
+
+displayBooks();
