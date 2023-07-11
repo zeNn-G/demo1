5c11f30a2922218cae42826047a2f4871aab403b
Erol Akyildiz <erol.akyildiz@sebit.com.tr>
2023-07-11 13:56:32 +0300

    Add Book

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 import "./style.css";
+
+const formSelector = document.querySelector("form");
+const bookListSelector = document.querySelector(".book-list");
+
+let bookList = [];
+
+const addBook = (book) => {
+  bookList = [...bookList, book];
+};
+
+formSelector.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const isbn = formData.get("isbn");
+
+  const book = {
+    title,
+    author,
+    isbn,
+  };
+
+  addBook(book);
+});
